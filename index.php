2897f342efcf00a996300432c646f280387ca7de
juan-sanchez <juan-phidias@laptop>
2016-08-17 11:25:01 -0500

    Added try catch.

--- index.php
+++ index.php
@@ -18,23 +18,66 @@ class Client {
 		$this->base_url = "https://api.themoviedb.org/3/";
 		$this->api_key  = "e32124517b8376484c88e5a11668e03b";
     }
-    public function getResults($query,$page = 1) {
-    	$url = $this->base_url."search/multi?query=".$query."&api_key=".$this->api_key."&language='en-US'&page=".$page;
-        return $this->guzzle->request('GET', $url)->getBody();
-    }
     public function getMovie($movie_id) {
-    	$url = $this->base_url."movie/".$movie_id."?api_key=".$this->api_key;
-        return $this->guzzle->request('GET', $url)->getBody();
+    	try {
+    		$url = $this->base_url."movie/".$movie_id."?api_key=".$this->api_key;
+        	return $this->guzzle->request('GET', $url)->getBody();
+    	} catch (GuzzleHttp\Exception\ClientException $e) {
+			return json_encode (json_decode ("{}"));
+		}
     }
     public function getPerson($person_id) {
-    	$url = $this->base_url."person/".$person_id."?api_key=".$this->api_key;
-        return $this->guzzle->request('GET', $url)->getBody();
+    	try {
+	    	$url = $this->base_url."person/".$person_id."?api_key=".$this->api_key;
+	        return $this->guzzle->request('GET', $url)->getBody();
+	    } catch (GuzzleHttp\Exception\ClientException $e) {
+			return json_encode (json_decode ("{}"));
+		}
     }
     public function getMoviesByActor($actor_id, $page = 1) {
     	$url = $this->base_url."discover/movie?with_cast=".$actor_id."&api_key=".$this->api_key."&language='en-US'&sort_by=release_date.desc&page=".$page;
-        return $this->guzzle->request('GET', $url)->getBody();
+    	try {
+        	return $this->guzzle->request('GET', $url)->getBody();
+        } catch (GuzzleHttp\Exception\ClientException $e) {
+			return json_encode (json_decode ("{}"));
+		}
+    }
+    public function getResults($query,$page = 1) {
+    	$url = $this->base_url."search/multi?query=".$query."&api_key=".$this->api_key."&language='en-US'&page=".$page;
+    	try {
+        	$json_result = json_decode($this->guzzle->request('GET', $url)->getBody());
+        	echo "entra try resutls";
+        } catch (GuzzleHttp\Exception\ClientException $e) {
+        	echo $e;
+			return json_encode (json_decode ("{}"));
+		}
+        foreach ($json_result->results as $result) {
+			if ($result->media_type=="person") {
+				$actor_id = $result->id;
+				$page = 0;
+				do {
+					$page++;
+					$movies   = json_decode($this->getMoviesByActor($actor_id, $page));
+						foreach ($movies->results as $movie) {
+							$movie->media_type = 'movie';
+							$movie->actor_name = $result->name;
+							$json_result->results[] = $movie;
+					}
+					if (!$movies->total_pages) {
+						$movies->total_pages =1;
+					}
+				} while ( $page != $movies->total_pages);
+			}
+		}
+		return json_encode($json_result);
     }
 }
+/*
+	$client   = new Client;
+	$query = "leonardo dicaprio";
+	$res      = $client->getResults($query,1);
+	echo $res;
+*/
 
 $app = new \Slim\App;
 
@@ -50,7 +93,6 @@ $app->get('/search/{query}/page/{page}', function (Request $request, Response $r
 	$response->getBody()->write($res);
     return $response;
 });
-
 
 $app->get('/movie/{movie_id}', function (Request $request, Response $response) {
 	$movie_id = $request->getAttribute('movie_id');
